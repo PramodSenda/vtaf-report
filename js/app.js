7f18fcaf5bbafacd95d6335f71cfb1be49e49df7
unknown <ihpathirage@CD-IHPATHIRAGE.Virtusa.com>
2015-02-24 19:15:25 +0530

    Commit changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+var testSuites;
+var testCases;
+
+var testSuiteLength;
+var testCasesLength;
+var testStepsLength;
+var testSuiteCount=0,testCaseCount=0,testStepCount=0;
+var testSuiteStatus=false;
+var testCaseStatus=false;
+var testStepStatus=false;
+
+
 
 function intializePieChart(failvalue,passvalue){
 var data = [
@@ -43,22 +55,137 @@ $("#screensize").text(jsonObject.activity[0].screenresolution);
 $("#duration").text(jsonObject.activity[0].duration);
 $("#totalwarning").text(jsonObject.activity[0].totalwarningcount);
 
-var testSuites = jsonObject.activity[0].activity[0].activity;
-console.log(testSuites);
+testSuites = jsonObject.activity[0].activity[0].activity;
+
+
  var tree = $("#tree");
-for(var testsuite=0;testsuite<testSuites.length;testsuite++){
+/*for(var testsuite=0;testsuite<testSuites.length;testsuite++){
   
  
   tree.append('<li>yo yo</li>');
 
 
 }
+*/
+testSuiteLength = testSuites.length 
+bulidtree(tree,testSuites,"testsuite");
+
+}
+
+
+//#########################################################
+
+
+
+
+
+
+
+function bulidtree(tree,treeitem,type){
+    
+
+
+if(type=="testsuite"){
+   if(testSuiteCount<testSuiteLength){
+    testSuiteStatus=true;
+    testSuite=treeitem[testSuiteCount];
+    tree.append('<li>'+testSuite.foldername+'</li>');
+    testSuiteCount++;
+    testCases = testSuite.activity;
+    testCaseLength = testCases.length
+    //console.log(testCases);
+    //console.log(testCaseLength);
+    
+   
+    bulidtree(tree,testCases,"testcase"); //array of testcase of testsuite
+   }
+}
+else if(type="testcase"){
+    if(testCaseCount<testCaseLength){
+      testCaseStatus=true;
+      testCase=treeitem[testCaseCount];
+      tree.append('<ul id="testcase"></ul>');
+      var testCaseNode = $("#testcase");
+      testCaseNode.append(testCase.testcasename);
+      testCaseCount++;
+      //teststeps = testcase.teststepArray;
+      //teststepslength = teststeps.length
+      //buildtree(teststeps,"teststep")
+
+     }
+}
+else if(type=="teststep"){
+ if(teststepcount<teststepslength){
+   teststepstatus=true;
+   teststep =  teststeps[teststepcount];                
+   teststepcount++;
+    if(teststep.type=="bComponent"){
+    createBcomponent(bComponent);
+    
+    
+    }else{
+      testcase.append(testStep);
+      
+    }
+ }
+}
+  if(testStepStatus){
+    testStepStatus=false;
+    buildtree(teststeps,"teststep");
+    
+    }
+  else if(testCaseStatus){
+    testCaseStatus=false;
+    bulidtree(tree,testCases,"testcase");
+    
+  }else if(testSuiteStatus){
+   testSuiteStatus=false;
+   bulidtree(tree,testSuites,"testsuite");
+   
+
+  }  
+    
+
+}
+
+
+function createBcomponent(bcomponent){
+    testcase.append(bcomponent) ;//as UL list
+    bcomponent.append(bcomponentteststep);
 
 
 }
+
+
+
+//##############################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
    
    getJson();
 });
+
+
+
+
+
+
+
