a1796e1189decd96e16b01971dfda65dcf91a347
Malindu Warapitiya <mwarapitiya@virtusa.com>
2015-02-24 16:00:36 +0530

    Restructure some code parts and move jquery-xml2json, jquery to bower dependencies.

--- js/app.js
+++ js/app.js
@@ -1,64 +1,66 @@
-
-function intializePieChart(failvalue,passvalue){
-var data = [
-    {
-        value: parseInt(failvalue),
-        color: "#F7464A",
-        highlight: "#FF5A5E",
-        label: "Fail"
+function intializePieChart(failvalue, passvalue) {
+    var data = [
+        {
+            value: parseInt(failvalue),
+            color: "#F7464A",
+            highlight: "#FF5A5E",
+            label: "Fail"
             },
-    {
-        value: parseInt(passvalue),
-        color: "#00b200",
-        highlight: "#00cc00",
-        label: "Pass"
+        {
+            value: parseInt(passvalue),
+            color: "#00b200",
+            highlight: "#00cc00",
+            label: "Pass"
             }
         ];
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myPieChart = new Chart(ctx).Pie(data);
-//console.log(data);
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myPieChart = new Chart(ctx).Pie(data);
 }
 
+function getJson() {
+    var jsonObject;
 
-
-function getJson(){
-    var jsonObject
-    $.get('report.html.data', function(xml){ 
+    $.get('report.html.data', function (xml) {
         jsonObject = $.xml2json(xml, true);
-       // console.log(jsonObject);
+        // console.log(jsonObject);
         createReport(jsonObject);
-    }); 
+    });
 }
 
-function createReport(jsonObject){
-  intializePieChart(jsonObject.activity[0].totalfailedcount, jsonObject.activity[0].totalsuccesscount);
+function createReport(jsonObject) {
 
-$("#exetime").text(jsonObject.activity[0].timestamp);
-$("#osystem").text(jsonObject.activity[0].osversion);
-$("#lang").text(jsonObject.activity[0].language);
-$("#totalerror").text(jsonObject.activity[0].totalerrorcount);
-$("#cname").text(jsonObject.activity[0].host);
-$("#screensize").text(jsonObject.activity[0].screenresolution);
-$("#duration").text(jsonObject.activity[0].duration);
-$("#totalwarning").text(jsonObject.activity[0].totalwarningcount);
+    intializePieChart(jsonObject.activity[0].totalfailedcount, jsonObject.activity[0].totalsuccesscount);
 
-var testSuites = jsonObject.activity[0].activity[0].activity;
-console.log(testSuites);
- var tree = $("#tree");
-for(var testsuite=0;testsuite<testSuites.length;testsuite++){
-  
- 
-  tree.append('<li>yo yo</li>');
+    $("#exetime").text(jsonObject.activity[0].timestamp);
+    $("#osystem").text(jsonObject.activity[0].osversion);
+    $("#lang").text(jsonObject.activity[0].language);
+    $("#totalerror").text(jsonObject.activity[0].totalerrorcount);
+    $("#cname").text(jsonObject.activity[0].host);
+    $("#screensize").text(jsonObject.activity[0].screenresolution);
+    $("#duration").text(jsonObject.activity[0].duration);
+    $("#totalwarning").text(jsonObject.activity[0].totalwarningcount);
 
+    var testSuites = jsonObject.activity[0].activity[0].activity,
+        tree = $("#tree");
 
+    for (var testsuite = 0; testsuite < testSuites.length; testsuite++) {
+
+//        var str = '<li><i class="glyphicon glyphicon-plus"></i> Test Suite Name';
+//            str += '<ul class="months"><li>Something here</li></ul>';
+//            str += '</li>';
+//        tree.append(str);
+    }
 }
 
+//expand/collapse ul list
+$('.tree ul').hide();
 
-}
+$('.months').click(function () {
+    $(this).find('ul').slideToggle();
+});
 
 // A $( document ).ready() block.
-$( document ).ready(function() {
-   
-   getJson();
+$(document).ready(function () {
+    getJson();
 });
